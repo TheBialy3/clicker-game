9c144ff4696a80748d5d4a463a36a243e008f14d
TheBialy3karbi <karbialasol@gmail.com>
2024-02-16 22:54:22 +0100

    items cost and smelter ArrayListsToGet added

--- src/managers/BeltManager.java
+++ src/managers/BeltManager.java
@@ -1,10 +1,20 @@
 package managers;
 
-public class BeltManager {
-    public BeltManager() {
+import buildings.Belt;
+import objects.Item;
 
+import java.util.ArrayList;
+
+import static helpz.Constants.BeltType.getBeltLvlSpeed;
+
+public class BeltManager {
+    private ArrayList<Item> itemsOnBelt=new ArrayList<>();
+    private ArrayList<Belt> belts=new ArrayList<>();
+    public BeltManager() {
     }
     public void moveItems(){
-        
+        for(Item item:itemsOnBelt){
+            item.moveItem(getBeltLvlSpeed(beltLvl),rotation);
+        }
     }
 }
